e0a828460e59aa05d74b47e337add4a1b05e3f27
marceloflp <mfelipedasilva2020@gmail.com>
2025-04-14 20:49:18 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/restaurante/reservasMesas/ReservasMesasApplication.java
@@ -0,0 +1,13 @@
+package br.com.restaurante.reservasMesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasMesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasMesasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/restaurante/reservasMesas/ReservasMesasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restaurante.reservasMesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasMesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
