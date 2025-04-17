e77d0e2fd95b4f946c597944562d5ca5bac9e687
marceloflp <mfelipedasilva2020@gmail.com>
2025-04-16 21:22:13 -0300

    Criacao do metodo insert em ClienteController

--- src/main/java/br/com/restaurante/reservasMesas/controllers/ClienteController.java
+++ src/main/java/br/com/restaurante/reservasMesas/controllers/ClienteController.java
@@ -1,11 +1,15 @@
 package br.com.restaurante.reservasMesas.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.restaurante.reservasMesas.entities.Cliente;
 import br.com.restaurante.reservasMesas.services.ClienteService;
@@ -24,5 +28,13 @@ public class ClienteController {
 	public ResponseEntity<List<Cliente>> findAll(){
 		List<Cliente> cliente = clienteService.findAll();
 		return ResponseEntity.ok(cliente);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> insert(@RequestBody Cliente cliente){
+		cliente = clienteService.insert(cliente);
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				 .path("/{id}").buildAndExpand(cliente.getId()).toUri();
+		 return ResponseEntity.created(uri).body(cliente);
 	}
 }
