8d89866ffefb8c57848e302ea9f0e1fc7b6221bb
marceloflp <mfelipedasilva2020@gmail.com>
2025-04-17 12:02:23 -0300

    Criacao do metodo delete em ClienteController

--- src/main/java/br/com/restaurante/reservasMesas/controllers/ClienteController.java
+++ src/main/java/br/com/restaurante/reservasMesas/controllers/ClienteController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +51,11 @@ public class ClienteController {
 	public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente cliente){
 		cliente = clienteService.update(id, cliente);
 		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/deletarCliente/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
